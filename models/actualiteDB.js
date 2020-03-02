c822be3f95bfb2027e55aa57d69039b84c0e19b7
Guillaume <guillaume.chebib@hotmail.fr>
2020-03-02 09:38:28 +0100

    actu ok + fix refresh token

--- models/actualiteDB.js
+++ models/actualiteDB.js
@@ -1,6 +1,36 @@
-let bd = require("../configV/database.js")
+let bd = require("../config/database.js")
 
 //bd.connect()
+
+async function editActualite(contenu,titre,id) {
+    try {
+        if(contenu===undefined || contenu==="" || titre===undefined || titre==="" || id===undefined || id===""){
+            return false
+        }
+        else {
+            let titre2 = titre.replace(" ", "")
+            const rep = await bd.client.query("UPDATE actualite SET contenuactualite=$1,titreactualite=$2,titreurlactualite=$3 WHERE idactualite=$4;",[contenu,titre,titre2,id])
+        }
+    }
+    catch (e) {
+        return false
+    }
+}
+
+async function addActualite(contenu,titre,id) {
+    try {
+        if(contenu===undefined || contenu==="" || titre===undefined || titre==="" || id===undefined || id===""){
+            return false
+        }
+        else {
+            let titre2 = titre.replace(" ", "")
+            const rep = await bd.client.query("INSERT INTO actualite(dateactualite,idutilisateur,contenuactualite,titreactualite,titreurlactualite) VALUES(NOW(),$1,$2,$3,$4);",[id,contenu,titre,titre2])
+        }
+    }
+    catch (e) {
+        return false
+    }
+}
 
 async function getActualite(){
     try{
@@ -28,5 +58,26 @@ async function getActualite1(id){
     }
 }
 
+async function getActualite2(id){
+    try{
+        const rep = await bd.client.query("SELECT * FROM actualite WHERE idactualite=$1",[id])
+        if(rep.rows[0]===undefined){
+            return false
+        }
+        else{
+            return rep.rows[0]
+        }
+    }
+    catch (e) {
+        return false
+    }
+}
+
+//editer
+//supprimer
+
 exports.getActualite = getActualite
 exports.getActualite1 = getActualite1
+exports.getActualite2 = getActualite2
+exports.editActualite = editActualite
+exports.addActualite = addActualite

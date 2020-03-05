28bbc135ae1ca223de0b76acc18fd88c7a570a8e
Guillaume <guillaume.chebib@hotmail.fr>
2020-03-05 16:45:49 +0100

    actu+stats

--- configV/database.js
+++ configV/database.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg')
 const client = new Client({
-    host: 'ec2-54-247-125-38.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    user: 'psnoovrrpcdwml',
-    password: '250f01a4cfa6d1104915956b31a633aa547af2318a846e783716ef05983bd3dd',
-    database: 'dalnvihgs5vc0'
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
 })
 
 async function connect(){

5f8bd40badb4ac3595982532aab493b7028e7155
Guillaume <guillaume.chebib@hotmail.fr>
2020-03-08 00:42:27 +0100

    secure

--- models/usersDB.js
+++ models/usersDB.js
@@ -1,4 +1,4 @@
-let bd = require("../configV/database.js")
+let bd = require("../config/database.js")
 const bcrypt = require("bcryptjs")
 let tok = require("../controlers/authController")
 let com=require("./commentaireDB")
